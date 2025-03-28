784d31e79c26e5e40bf868995c29032c5340f092
Vinicius Andrade <viniciusandradeevangelista2003@gmail.com>
2025-03-28 20:15:21 -0300

    Revert "Historico"
    This reverts commit de2b7f652c45bf5235aae49bc252855226205b4a.

--- src/Main.java
+++ src/Main.java
@@ -177,7 +177,7 @@ public class Main {
 //
 //                    var professor_ = gestaoEscolar.getListaProfessores().stream()
 //                            .filter(p -> p.getMatricula() == matricula)
-//                            .findFirst(0)
+//                            .findFirst()
 //                            .orElse(null);
 
                     var disciplina_ = gestaoDisciplina.pegarDisciplinaPorId(id);
