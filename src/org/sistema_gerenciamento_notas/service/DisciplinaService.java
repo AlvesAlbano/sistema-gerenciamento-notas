a885a6aa2240f3e2fc3c8b831e461fbcab9efc35
felip0za <felipechavesb1@gmail.com>
2025-05-04 19:23:53 -0300

    commit

--- src/org/sistema_gerenciamento_notas/service/DisciplinaService.java
+++ src/org/sistema_gerenciamento_notas/service/DisciplinaService.java
@@ -2,18 +2,12 @@ package org.sistema_gerenciamento_notas.service;
 
 import org.sistema_gerenciamento_notas.model.Disciplina;
 import org.sistema_gerenciamento_notas.repository.DisciplinaRepository;
-import org.sistema_gerenciamento_notas.strategy.ListagemStrategy;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DisciplinaService implements DisciplinaRepository {
     private List<Disciplina> listaDisciplinas = new ArrayList<>();
-    private ListagemStrategy listagemStrategy;
-
-    public void setListagemStrategy(ListagemStrategy listagemStrategy) {
-        this.listagemStrategy = listagemStrategy;
-    }
 
     public List<Disciplina> getListaDisciplinas() {
         return listaDisciplinas;
@@ -30,27 +24,21 @@ public class DisciplinaService implements DisciplinaRepository {
 
     @Override
     public void removerDisciplina(int idDisciplina) {
-        listaDisciplinas.removeIf(
-                disciplina -> disciplina.getIdDisciplina() == idDisciplina
-        );
+        listaDisciplinas.removeIf(d -> d.getIdDisciplina() == idDisciplina);
     }
 
     @Override
     public void listarDisciplinas() {
         System.out.printf("%sLista Disciplinas%s\n", "\u001B[34m", "\u001B[0m");
-        if (listagemStrategy != null) {
-            listagemStrategy.listar(listaDisciplinas);
-        } else {
-            System.out.println("Nenhuma estratégia de listagem definida.");
+        for (Disciplina d : listaDisciplinas) {
+            System.out.printf("ID: %s | Nome: %s\n", d.getIdDisciplina(), d.getNomeDisciplina());
         }
     }
 
     @Override
     public Disciplina pegarDisciplinaPorId(int idDisciplina) {
-        for (Disciplina disciplina : listaDisciplinas) {
-            if (disciplina.getIdDisciplina() == idDisciplina) {
-                return disciplina;
-            }
+        for (Disciplina d : listaDisciplinas) {
+            if (d.getIdDisciplina() == idDisciplina) return d;
         }
         return null;
     }
